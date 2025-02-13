f8120022fdaec4c86bd64b04fd89ba124a59bb8e
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-02-13 16:22:37 +0530

    First Program

--- First.java
+++ First.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class First{
     public static void main(String args[]){
-        System.out.println("Hello World");
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("phy " + marks[0]);
+        System.out.println("chem " + marks[1]);
+        System.out.println("maths " + marks[2]);
+
     }
 }
