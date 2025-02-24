a8e34d10ef16621b48a0ac916f1c00732a5bc8db
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-02-24 16:15:00 +0530

    Kadanes algorithm

--- ARRAYS/printSubarraySum.java
+++ ARRAYS/printSubarraySum.java
@@ -49,9 +49,27 @@ public class printSubarraySum {
         }
         System.out.println("max sum is : " + maxSum);
     }
+
+    //Kadanes Algorithm
+    public static void kadanes(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i=0; i<numbers.length; i++) {
+            currSum = currSum + numbers[i];
+
+            if(currSum < 0) {
+                currSum = 0;
+            }
+          maxSum = Math.max(currSum, maxSum);
+        }
+        System.out.println("max sum is : " + maxSum);
+    }
+
+
     public static void main(String[] args) {
-        int numbers[] = {2,4,6,8,10};
-        prefixSum(numbers);
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        kadanes(numbers);
     }
 
 }
