c7dee951519328e4c1fc45e5dfd5d9abbafcc7b8
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-02-27 14:27:02 +0530

    Optimized way

--- ARRAYS/largestNumber.java
+++ ARRAYS/largestNumber.java
@@ -4,13 +4,20 @@ import java.util.*;
 public class largestNumber {
 
     public static int getLargest(int numbers[]) {
+
+        //Time complexity - O(n).
         int largest = Integer.MIN_VALUE;
         for(int i=0; i<numbers.length; i++) {
-            if(largest < numbers[i]) {
+            if(numbers[i] > largest) {
                 largest = numbers[i];
             }
         }
         return largest;
+
+
+    //    Most optimized way - TC - O(1).
+    //    Arrays.sort(numbers);
+    //    return numbers[numbers.length-1];
     }
     public static void main(String args[]) {
         int numbers[] = {1,2,6,3,5};
