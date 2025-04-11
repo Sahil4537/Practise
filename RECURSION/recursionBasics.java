b7caff995e2c6f3dc4a74cbbfd8dcb29b304394a
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-04-12 01:58:28 +0530

    Print in Increasing order

--- RECURSION/recursionBasics.java
+++ RECURSION/recursionBasics.java
@@ -1,6 +1,8 @@
 
 
 public class recursionBasics {
+
+    //Print in Decreasing order.
     public static void printDec(int n) {
         if(n == 1) {
             System.out.println(n);
@@ -9,8 +11,19 @@ public class recursionBasics {
         System.out.print(n + " ");
         printDec(n-1);
     }
+
+    //Print in Increasing order.
+    public static void printInc(int n) {
+        if(n==1) {
+            System.out.print(n + " ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n + " ");
+    }
     public static void main(String args[]) {
         int n=10;
-        printDec(10);
+      //  printDec(10);
+        printInc(10);
     }
 }
