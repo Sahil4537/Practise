752444d4aec516b51176a9a5dcb0c9a338d59848
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-04-12 02:11:52 +0530

    Print factorial

--- RECURSION/recursionBasics.java
+++ RECURSION/recursionBasics.java
@@ -21,9 +21,21 @@ public class recursionBasics {
         printInc(n-1);
         System.out.print(n + " ");
     }
+
+    //Print Factorial
+    public static int printFact(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        int fnm1 = printFact(n-1);
+        int fn = n * printFact(n-1);
+        return fn;
+    }
+
     public static void main(String args[]) {
         int n=10;
       //  printDec(10);
-        printInc(10);
+      //  printInc(10);
+      System.out.println(printFact(5));
     }
 }
