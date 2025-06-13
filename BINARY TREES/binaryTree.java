2b30288bcc5885433a0ef4e4d4315c8b4cf8a759
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-06-13 14:56:27 +0530

    preorder traversal

--- BINARY TREES/binaryTree.java
+++ BINARY TREES/binaryTree.java
@@ -26,11 +26,23 @@ public class binaryTree {
 
             return newNode;
         }
+
+    //preorder traversal
+        public static void preorder(Node root) {
+            if(root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
     public static void main(String args[]) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        //System.out.println(root.data);
+
+        tree.preorder(root);
     }
 }
