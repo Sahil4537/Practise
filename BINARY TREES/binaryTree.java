d14518ec3f23076fd6a3ef14b30aee181ab63773
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-06-15 23:53:38 +0530

    level order traversal

--- BINARY TREES/binaryTree.java
+++ BINARY TREES/binaryTree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class binaryTree {
     static class Node {
@@ -50,12 +51,42 @@ public class binaryTree {
     //postorder traveresal
         public static void postorder(Node root){
             if(root == null) {
-                return;
+                return ;
             }
             postorder(root.right);
             postorder(root.left);
             System.out.print(root.data);
         }
+
+    //Level order traversal
+    public static void levelOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()) {
+            Node currNode = q.remove();
+            if(currNode == null) {
+                System.out.println();
+                if(q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if(currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
     }
     public static void main(String args[]) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -65,6 +96,7 @@ public class binaryTree {
 
         //tree.preorder(root);
         //tree.inorder(root);
-        tree.postorder(root);
+        //tree.postorder(root);
+        tree.levelOrder(root);
     }
 }
